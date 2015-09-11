9a5a93bc632ce0d5374673ae9c9c28fe7e204c27
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-11 13:29:26 -0700

    Remove more unnecessary dashboard widgets including welcome panel.

--- class-wp-clean.php
+++ class-wp-clean.php
@@ -177,9 +177,13 @@ if ( ! class_exists( 'WP_Clean' ) ) {
     // Remove unnecessary dashboard widgets
     function remove_dashboard_widgets() {
       remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal');
+      remove_meta_box('dashboard_quick_press', 'dashboard', 'side');
+      remove_meta_box('dashboard_right_now', 'dashboard', 'side');
+      remove_meta_box('dashboard_activity', 'dashboard', 'side');
       remove_meta_box('dashboard_plugins', 'dashboard', 'normal');
       remove_meta_box('dashboard_primary', 'dashboard', 'normal');
       remove_meta_box('dashboard_secondary', 'dashboard', 'normal');
+      remove_action( 'welcome_panel', 'wp_welcome_panel' );
     }
 
   } // End of WP_Clean class
