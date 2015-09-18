3b471bd89f62b2f25a578dc4707dd6c633720bfe
Daniel Hewes <daniel@danimalweb.co.uk>
2015-09-18 14:56:29 -0700

    Correct function name.

--- modules/disable-posts.php
+++ modules/disable-posts.php
@@ -6,4 +6,4 @@ function action_remove_menus() {
   remove_menu_page( 'edit.php' );
 }
 
-add_action( 'wp_before_admin_bar_render', __NAMESPACE__ . '\\admin_bar_render' );
+add_action( 'wp_before_admin_bar_render', __NAMESPACE__ . '\\action_remove_menus' );
