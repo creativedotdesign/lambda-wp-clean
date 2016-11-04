0c171516aaee37d7df3c9f326b616700ee71d86d
Daniel Hewes <daniel@lambdacreatives.com>
2016-11-03 21:58:31 -0700

    Move multi filter / actions functions in the main plugin file.

--- wp-clean.php
+++ wp-clean.php
@@ -8,8 +8,10 @@
  * Author URI: http://lambdacreatives.com/
  */
 
-require_once 'wp-multifilter.php';
 
+/**
+ * Module include based on add_theme_support value
+ */
 add_action('after_setup_theme', function() {
   foreach (glob(__DIR__ . '/modules/*.php') as $file) {
     if (current_theme_supports('wp-clean-' . basename($file, '.php'))) {
@@ -17,3 +19,29 @@ add_action('after_setup_theme', function() {
     }
   }
 });
+
+
+/**
+ * Hooks a single callback to multiple tags
+ */
+function add_filters($tags, $function, $priority = 10, $accepted_args = 1) {
+  foreach ((array) $tags as $tag) {
+    add_filter($tag, $function, $priority, $accepted_args);
+  }
+}
+
+
+/**
+ * Add multiple actions to a closure
+ *
+ * @param $tags
+ * @param $function_to_add
+ * @param int $priority
+ * @param int $accepted_args
+ *
+ * @return bool true
+ */
+function add_actions($tags, $function_to_add, $priority = 10, $accepted_args = 1) {
+  //add_action() is just a wrapper around add_filter(), so we do the same
+  return add_filters($tags, $function_to_add, $priority, $accepted_args);
+}
